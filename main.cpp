427ce3cff8b179af9ae40bbbe2ddde09f6657be0
MFEshinhyunjin <42334069+MFEshinhyunjin@users.noreply.github.com>
2021-07-18 21:07:45 +0900

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,96 @@
+
+#include "NS.hpp"
+
+
+namespace NS
+    {     
+     template<typename T>
+     class CLS    
+     {     public:       
+     ....    
+     virtual void blink() {}     
+     CLS(int A = 0, int B =0);     
+     int A() const;     
+     int B() const;     
+     void setA(int newVar);     
+     void setB(int newVar);          
+     
+     struct CLS2DATA     
+     {     CLS left;     CLs right;     };          
+     class CLS2     
+     {     
+     public:     
+     const CLS& leftFUNC() const {return pDatat->left;}     
+     const CLS& rightFUNC() const {return pDatat->right;}     
+     
+     private:     
+     std::tr1::shared_ptr<CLSDATA> pData;     
+     };     
+     // vs of CLS2     
+     class CLS2TEMP { };     
+     const CLS2 CLS2FUNC(const CLS2TEMP& obj); 
+     // up : class, down func with same role for showing eg     
+     CLS2TEMP *pgo; // pgo pointing CLS2TEMP    
+     const CLS *pLeft = &(CLS2FUNC(*pgo).pLeft());//=CLS2FUNC's left of pointer from pgo pointing CLS2TEMP     
+     };
+     // out of class, non-member functions     
+     void FUNC(CLS& var);     
+     const CLS operator*(const CLS& rhs, const CLS& lhs); // when type converting
+     template<typename T>     
+     void swap(CLS<T>& a,_swap CLS<T>& b_swap)     
+     {     
+     a_swap..swap(b_swap)     
+     };          
+     void doSomething(T& obj1, T& obj2)     
+     {     
+     using std::swap;     
+     swap(obj1, obj2) 
+     // X std::swap(obj1, obj2)     
+     }
+
+     // Children Class     
+     class CLSCHILD : public CLS     
+     {     
+     public:     
+     virutal void blink() { ... } ;     
+     virtual void FUNCCHILD()     
+     {     
+     CLS::FUNCCHILd();     
+     }     
+     };     
+     
+     typedef std::vector<std::tr1::shared_ptr<CLS>>VPW;     
+     VPW winPtrs; // all pointers from CLS     
+     for(VPW::iterator iter = winPtrs.begin(); iter != winPtrs.end(); ++iter)     
+     {     
+     (*iter)->blink();     
+     }
+     for (int i =0; i<n ; ++i)
+     {     
+     CLS w( ~ i); // no outside declaration : CLS w 
+     }
+     
+     // 29 : exception safety //
+     class Menu
+     {
+     public:
+     void change(std::istream& memberVar);
+     
+     private:
+     Mutex mutex;
+     TYPE1 *element;
+     int changes; // change numbers
+     
+     void Menu::change(std::istream& memberVar)
+     {
+     Lock ml(&mutex); // **
+     delete element;
+     ++changes;
+     element = new TYPE1(memberVar);
+     unlock(&mutex);
+     
+ }
+
+
+
+
