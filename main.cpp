cbd4fa4580648e9c246336dd532f3ef504521e32
BeAmericanCitizen <42334069+FinancialEngineerLab@users.noreply.github.com>
2021-07-23 11:10:32 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -630,7 +630,45 @@ namespace NS
            NewHandlerHolder h(std::set_new_handler(currentHandler));
            return ::operator new(size);
        }
+       
+       template<typename T>
+       class NewHandlerSupport
+       {
+           public:
+           static std::new_handler set_new_handler(std::new_handler p) throw();
+           static void * operator new(std::size_t size) throw(std::bad_alloc);
+           
+           private:
+           static std::new_handler curentHandler;
+       };
                      
+       template<typename T>
+       std::new_handler
+       NewHandlerSupport<T>::set_new_handler(std::new_handler p) throw()
+       {
+           std::new_handler oldHandler = currentHandler;
+           currentHandler = p;
+           return oldHandler;
+       }
+       
+       template<typename T>
+       void* NewHandlerSupport<T>::operator new(std::size_t size) throw(std::bad_alloc)
+       {
+           NewHandlerHolder h(std::set_new_handler(currentHandler));
+           return ::operator new(size);
+       }
+                     
+       template<typename T>
+       std::new_handler NewHandlerSupport<T>::currentHandler = 0;
+                     
+       class Widget: public NewHandlerSUpport<Widget>
+       {
+           ...
+       };
+                         
+       Widget *pw2 = new (std::nothrow) Widget;
+                         
+                         
                          
                          
 }
